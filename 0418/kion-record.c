ae004c7258d47d9ecd8f67cd349bee2737c2ef44
kou e <koue@kounoMacBook-Pro.local>
2023-04-19 11:23:59 +0900

    0419 homework

--- 0418/kion-record.c
+++ 0418/kion-record.c
@@ -1,51 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct kisyou { /* 気温データを管理するレコード */
-    int month ;
-    int day ;
-    int hour ;
-    double kion ;
-} ;
+struct kisyou
+{ /* 気温データを管理するレコード */
+    int month;
+    int day;
+    int hour;
+    double kion;
+};
 
-#define	MAXFILENAME 100
 /* 実験のため配列のサイズを少し小さく */
 #define ARRAYSIZE 1000
 
+int readfile(char filename[], struct kisyou kisyoudata[])
+{
+    FILE *fp;
+    int size;
+    int month, day, hour;
+    double data;
+    char file[100];
+
+    /* ファイルのオープン：ファイル名 filename と入力用の利用 "r" を指定 */
+    if ((fp = fopen(filename, "r")) == NULL)
+    {
+        /* 以下の２行はファイルがオープンできなかった時の処理 */
+        fprintf(stderr, "Error: File Open\n");
+        exit(1); /* プログラムをここで終了させる */
+    }
+
+    size = 0;
+    while (fscanf(fp, "%d,%d,%d,%lf", &month, &day, &hour, &data) != EOF)
+    {
+        if (size >= ARRAYSIZE)
+        {
+            fprintf(stderr, "Error: 配列の容量が不足しています。\n");
+            exit(2);
+        }
+        /* すべてのデータを構造体のメンバに保存 */
+        kisyoudata[size].month = month;
+        kisyoudata[size].day = day;
+        kisyoudata[size].hour = hour;
+        kisyoudata[size].kion = data;
+        (size)++;
+    }
+
+    fclose(fp);
+
+    return size;
+}
+
 int main(void)
 {
-    char filename[MAXFILENAME] ;
-    FILE *fp ;
-    int month, day, hour ;
-    double data ;
-    struct kisyou kisyoudata[ARRAYSIZE] ; /* 構造体の配列を追加 */
-    int size, i ;
+    struct kisyou kisyoudata[ARRAYSIZE]; /* 構造体の配列を追加 */
+    char filename[100];
+    int size, i;
 
-     fprintf(stderr, "データファイル名：") ;
-    scanf("%s", filename) ; /* 端末からファイル名を入力 */
-    
-    if ((fp = fopen("kion-20180401.csv", "r")) == NULL) {
-	fprintf(stderr, "Error: File Open\n") ;
-	exit(1) ;
+    /* プロンプトの出力とファイル名の入力 */
+    fprintf(stderr, "データファイル名：");
+    scanf("%s", filename); /* 端末からファイル名を入力 */
+
+    size = readfile(filename, kisyoudata);
+
+    for (i = 0; i < size; i++)
+    {
+        printf("%d月%d日%d時 %.1f\n", /* 構造体のメンバを出力 */
+               kisyoudata[i].month, kisyoudata[i].day,
+               kisyoudata[i].hour, kisyoudata[i].kion);
     }
 
-    size = 0 ;
-    while (fscanf(fp, "%d,%d,%d,%lf", &month, &day, &hour, &data) != EOF) {
-	/* すべてのデータを構造体のメンバに保存 */
-	kisyoudata[size].month = month ;
-	kisyoudata[size].day = day ;
-	kisyoudata[size].hour = hour ;
-	kisyoudata[size].kion = data ;
-	size++ ;
-    }
-
-    fclose(fp) ;
-
-    for (i = 0; i < size; i++) {
-	printf("%d月%d日%d時 %.1f\n", /* 構造体のメンバを出力 */
-		kisyoudata[i].month, kisyoudata[i].day,
-		kisyoudata[i].hour, kisyoudata[i].kion) ;
-    }
-
-    return 0 ;
+    return 0;
 }
