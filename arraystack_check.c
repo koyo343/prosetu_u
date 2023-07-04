c7db1fac0e6b10ce4dd585edc2ae79d9921dde83
koyo343 <105850444+koyo343@users.noreply.github.com>
2023-07-04 11:30:05 +0900

    :fired:

---
+++ arraystack_check.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "arraystack.h"
+int main(void){
+    STACK *sp1,*sp2 ;
+
+    sp1=newStack ( ) ;
+    sp2=newStack ( ) ;
+    printf("Stack1:Element %d\n",stackElement(sp1));
+    stackPush (sp1,1) ;
+    stackPush ( sp1 , 2 ) ;
+    stackPush ( sp1 , 4 ) ;
+    stackPush ( sp1 , 8 ) ;
+    printf("Stack2:Element %d\n",stackElement(sp2));
+    stackPush ( sp2 , 1 0 0 ) ;
+    stackPush ( sp2 , 2 0 0 ) ;
+    stackPush ( sp2 , 4 0 0 ) ;
+    printf("Stack1:Element %d\n",stackElement(sp1));
+    printf("Stack2:Element %d\n",stackElement(sp2));
+
+    while(!stackIsEmpty(sp1))
+        printf("Stack1:%d\n",stackPop(sp1));
+    while(!stackIsEmpty(sp2))
+        printf("Stack2:%d\n",stackPop(sp2));
+    printf("Stack1:Element %d\n",stackElement(sp1));
+    printf("Stack2:Element %d\n",stackElement(sp2));
+
+    return 0 ;
+}
